a7bf98b32574870c83bd4a40d75b0351871670e8
Birch door <me>
2026-02-22 12:59:21 +0100

    idk

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from akordy import song
 
 test = song()
 i = input("url zadej: ")
-if i:
-    test.url = i
-else:
-    test.url = "https://pisnicky-akordy.cz/vypsana-fixa/schovana"
+test.url = "https://pisnicky-akordy.cz/vypsana-fixa/schovana"
 test.load()
 test.curl()
 test.save()
